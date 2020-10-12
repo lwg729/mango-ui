3cd2f1690a041327eb8050320376b170f4d4b78f
feature_liwg <liwg@hyjx.com>
2020-10-12 16:05:00 +0800

    使用mock模拟后台数据

---
+++ src/mock/mock.js
@@ -0,0 +1,11 @@
+import Mock from 'mockjs'
+Mock.mock('http://localhost:8080/user',{
+  'name': '@name',  //随机生成姓名
+  'email': '@email', //随机生成邮件
+  'age|1-10': 5, //年龄在1-10岁之间
+})
+Mock.mock('http://localhost:8080/menu',{
+  'id': '@increment', //自增
+  'name': 'menu',  //名字为menu
+  'order|1-20': 5,//排序在1-20之间
+})
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,30 @@
 <template>
   <div class="page">
     <h2>Home Page</h2>
+    <el-button type="primary" @click="testAxios()">测试Axios掉用</el-button>
+    <el-button type="primary" @click="getUser()">获得用户信息</el-button>
+    <el-button type="primary" @click="getMenu()">获得菜单信息</el-button>
   </div>
 </template>
 
 <script>
-
+import axios from "axios"
+import mock from '../mock/mock.js'
+export default {
+  name: 'Home',
+  methods: {
+    testAxios(){
+      axios.get('http://localhost:8080')
+      .then(res=>{alert(res.data)})
+    },
+    getUser(){
+      axios.get('http://localhost:8080/user').then(
+        res=> {alert(JSON.stringify(res.data))}
+      )
+    },
+    getMenu(){
+      axios.get('http://localhost:8080/menu').then(res=>{alert(JSON.stringify(res.data))})
+    }
+  }
+}
 </script>
