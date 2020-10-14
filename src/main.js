b5eb5f918addb4e8448c8d8f1869465c46297030
lwg729 <1098372951@qq.com>
2020-10-14 15:23:56 +0800

    登录流程完善

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import api from './http'
+import i18n from "./i18n";
 import global from './utils/global'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
@@ -15,5 +16,6 @@ Vue.prototype.global = global // 挂载全局配置模块
 new Vue({
   el: '#app',
   router,
+  i18n,
   render: h => h(App)
 })
